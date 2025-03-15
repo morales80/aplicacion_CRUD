679757ff6a840a65b10f2c40346f86be4633fdfb
MiguelPortes <miguelgompor167@gmail.com>
2025-03-14 18:14:23 -0600

    Se agrego el get

--- server.js
+++ server.js
@@ -3,15 +3,32 @@ const app = express()
 const port = 3000
 const conexion = require ('./db/conexion.js')
 
-app.get('/', (req, res) => {
-  res.send(' Dashboard')
-})
+// Método GET para obtener un producto por ID
+app.get('/productos/:id', (req, res) => {
+  const producto = productos.find(p => p.id === parseInt(req.params.id));
+  if (!producto) return res.status(404).json({ mensaje: 'Producto no encontrado' });
+  res.json(producto);
+});
 
-app.post('/usuarios', (req, res) =>{
-  res.send('agregar Usuarios')
-})
+
+// Método GET para obtener todos los usuarios
+app.get('/usuarios', (req, res) => {
+  res.json(usuarios);
+});
+
+// Método PATCH para actualizar un usuario por ID
+app.patch('/usuarios/:id', (req, res) => {
+  const usuario = usuarios.find(u => u.id === parseInt(req.params.id));
+  if (!usuario) return res.status(404).json({ mensaje: 'Usuario no encontrado' });
+
+  res.json(usuario);
+});
 
 
-app.listen(port, () => {
+
+app.listen(port, (err) => {
+  if (err) console.log(err);
   console.log(`servidor corriendo en el puerto: ${port}`)
-})
+});
+
+
